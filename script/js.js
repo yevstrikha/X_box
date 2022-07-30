1e2fd149cefdd08bde2bf7bbd5d1231f75d1104b
yevstrikha <yevstrikha@gmail.om>
2022-07-29 21:01:43 -0400

    add files

---
+++ script/js.js
@@ -0,0 +1,16 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+
+        1000:{
+            items:1,
+            nav:true,
+        }
+    }
+})
