782d228d9faec3eb09b666503eb5df4121f32c91
yevstrikha <yevstrikha@gmail.om>
2022-07-30 22:57:29 -0400

    add new carousel

---
+++ script/new_carousel.js
@@ -0,0 +1,15 @@
+$('.exclusive-carousel').owlCarousel({
+    loop:true,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:3.7
+        }
+    }
+});
