91c199abd16ffbcfb9e998b5852c4d8628d90925
yevstrikha <yevstrikha@gmail.om>
2022-08-04 21:30:06 -0400

    menu media

--- script/new_carousel.js
+++ script/new_carousel.js
@@ -1,16 +1,15 @@
 $('.exclusive-carousel').owlCarousel({
     loop:true,
     nav:true,
-    autoplay:true,
-    autoplayTimeout:2500,
-    autoplayHoverPause:true,
     responsive:{
         0:{
             items:1,
         },
         600:{
             items:2,
-            margin: 30
+            margin: 30,
+            autoplayTimeout:2500,
+
         },
         1000:{
             items:3.9,
